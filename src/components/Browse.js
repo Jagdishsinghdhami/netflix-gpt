b0033d03742304490b1e7f1768375b5ee78f7a25
Jagdish Singh Dhami <jagdish.dhami@matpatra.com>
2024-08-25 13:28:27 +0530

    login and sign up form html done.

---
+++ src/components/Browse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Browse = () => {
+    return (
+        <div>
+            <h2>this is the browse component</h2>
+        </div>
+    )
+}
+
+export default Browse
